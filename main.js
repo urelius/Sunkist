081ce89e6667bf2a50fbaf167741b956bed14f35
Adam Smith <axfanwashere@gmail.com>
2022-05-17 18:21:48 -0700

    feat: setup initial app

---
+++ main.js
@@ -0,0 +1,40 @@
+const { app, BrowserWindow, globalShortcut} = require('electron')
+
+
+let flyffWin;
+
+function createWindow () {
+  flyffWin = new BrowserWindow({
+    width: 800,
+    height: 600,
+    autoHideMenuBar: true,
+    show: false,
+    icon: "icon.png"
+  })
+
+  flyffWin.loadURL('https://universe.flyff.com/play')
+
+  flyffWin.once('ready-to-show', () => {
+    flyffWin.show()
+  })
+}
+
+app.whenReady().then(() => {
+    globalShortcut.register('Esc', () => {
+      overlay.show();
+    })
+  }).then(createWindow)
+
+
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
