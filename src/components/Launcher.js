28f730d840be115652a3fb239e09331b3a1cf151
Nikolaj Rjabenko <rjabenko@me.com>
2022-07-30 00:07:33 +0200

    moved bgs images to imgs/ added class images

--- src/components/Launcher.js
+++ src/components/Launcher.js
@@ -1,49 +1,68 @@
-import React, { useEffect, useState } from 'react';
-import { Users, Maximize2, Minimize2 } from 'react-feather';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { Settings } from "react-feather";
+import { Link } from "react-router-dom";
 
-import logo from '../flyffu.png'
+import logo from "../flyffu.png";
 
 const Launcher = () => {
-    const [currentProfile, setCurrentProfile] = useState()
-    const [profiles, setProfiles] = useState([])
+  const [currentProfile, setCurrentProfile] = useState();
+  const [profiles, setProfiles] = useState([]);
 
-    useEffect(() => {
-        const getProfiles = async () => {
-            const profiles = await window.api.getProfiles();
-            setProfiles(profiles);
-            setCurrentProfile(profiles[0] || '')
-        }
-        getProfiles()
-    }, []);
+  useEffect(() => {
+    const getProfiles = async () => {
+      const profiles = await window.api.getProfiles();
+      setProfiles(profiles);
+      setCurrentProfile(profiles[0] || "");
+    };
+    getProfiles();
+  }, []);
 
-    const changeProfile = (profile) => {
-        setCurrentProfile(profile);
-    }
+  const changeProfile = (profile) => {
+    setCurrentProfile(profile);
+  };
 
-    return (
-        <div id="launcher" className="flex-1">
-            <div className='flex-1 text-center'>
-                <img src={logo} className="flyffLogo block m-auto pr-3" alt="Flyff Universe" />
-                <div className="m-auto block mt-6">
-                    <select id="profile" value={currentProfile} onChange={(e) => changeProfile(e.target.value)} className="text-slate-900 p-1 inline-block w-40 rounded-l">
-                        {profiles.map((profile, index) => {
-                            return <option key={index} value={profile}>{profile}</option>
-                        }
-                        )}
-                    </select>
-                    <Link to="/profiles">
-                        <Users className="bg-sky-600 hover:bg-sky-700 rounded-r p-2 inline-block mb-1" size={36}/>
-                    </Link>
-                </div>
-                <p>
-                    <button type="button" className="bg-sky-600 p-2 w-52 rounded mt-6 hover:bg-sky-700" onClick={() => window.api.launchGame(currentProfile)}>
-                        Play
-                    </button>
-                </p>
-            </div>
+  return (
+    <div id="launcher" className="flex-1">
+      <div className="flex-1 text-center">
+        <img
+          src={logo}
+          className="flyffLogo block m-auto pr-3"
+          alt="Flyff Universe"
+        />
+        <div className="m-auto block mt-6">
+          <select
+            id="profile"
+            value={currentProfile}
+            onChange={(e) => changeProfile(e.target.value)}
+            className="text-slate-900 p-2 inline-block w-40 rounded-l"
+          >
+            {profiles.map((profile, index) => {
+              return (
+                <option key={index} value={profile}>
+                  {profile}
+                </option>
+              );
+            })}
+          </select>
+          <Link to="/settings">
+            <Settings
+              className="bg-sky-600 hover:bg-sky-700 rounded-r p-2 inline-block mb-2"
+              size={36}
+            />
+          </Link>
         </div>
-    );
-}
+        <p>
+          <button
+            type="button"
+            className="bg-sky-600 p-2 w-52 rounded mt-6 hover:bg-sky-700"
+            onClick={() => window.api.launchGame(currentProfile)}
+          >
+            Play
+          </button>
+        </p>
+      </div>
+    </div>
+  );
+};
 
 export default Launcher;
---
+++ src/imgs/classes/index.js
@@ -0,0 +1,31 @@
+// import all images
+
+import iAcrobat from './iAcrobat.png';
+import iMagician from './iMagician.png';
+import iAssist from './iAssist.png';
+import iMercenary from './iMercenary.png';
+import iRanger from './iRanger.png';
+import iJester from './iJester.png';
+import iRingmaster from './iRingmaster.png';
+import iBillposter from './iBillposter.png';
+import iElementor from './iElementor.png';
+import iPsykeeper from './iPsykeeper.png';
+import iBlade from './iBlade.png';
+import iKnight from './iKnight.png';
+
+
+// export classes
+export default [
+    iAcrobat,
+    iAssist,
+    iMagician,
+    iMercenary,
+    iRanger,
+    iJester,
+    iRingmaster,
+    iBillposter,
+    iElementor,
+    iPsykeeper,
+    iBlade,
+    iKnight,
+];
